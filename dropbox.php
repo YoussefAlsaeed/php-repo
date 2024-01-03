e8f3fdb0c3be8094b9dc6ec3b4cc96d7c02bf658
Youssef Alsaeed <yousefalsaeed2002@gmail.com>
2024-01-03 22:57:06 +0200

    Create dropbox.php

---
+++ dropbox.php
@@ -0,0 +1,36 @@
+<!-- CREATE DYNAMIC DROPBOX USING PHP -->
+
+<html>
+
+<body>
+<?php
+
+$arr = array("Sunday", "Monday" , "Tuesday" , "Wednesday" , "Thursday", "Friday","Saturday");
+
+echo " <form>";
+echo "<select>";
+
+foreach($arr as $day)
+{
+    echo "<option>" . $day . "</option>";
+}
+
+echo "</select>";
+echo "<br><br>";
+
+echo "<select>";
+
+for($i = 1950 ; $i <= 2050 ; $i++)
+{
+    echo "<option>" . $i . "</option>";
+}
+
+echo "</select>";
+
+
+
+echo " </form>";
+
+?>
+</body>
+</html>
